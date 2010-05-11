9d91b854a5fcb67bd68eb421a826ab3ae7d27afb
alejandro.asenjo <alejandro.asenjo@2500659b-82e6-3a7b-9723-c36c4ad48c0c>
2010-05-11 16:45:15 +0000

    Added session and app init code

--- DataMiningApp/Global.asax.cs
+++ DataMiningApp/Global.asax.cs
@@ -9,12 +9,25 @@ namespace DataMiningApp
 {
     public class Global : System.Web.HttpApplication
     {
+        Registry.Registry appRegistry;
+        Registry.Registry sessionRegistry;
         int stepid;
+
 
         void Application_Start(object sender, EventArgs e)
         {
             // Code that runs on application startup
+            appRegistry = new Registry.Registry(Application);
 
+            //Add algorithms
+            Analysis.Analysis myPCA1 = new Analysis.PCA.PCA("Principal Component Analysis1");
+            appRegistry.registerAlgorithm(myPCA1);
+            myPCA1.setDocumentationURL(new Uri("http://en.wikipedia.org/wiki/Principal_component_analysis"));
+            Analysis.Analysis myPCA2 = new Analysis.PCA.PCA("Principal Component Analysis2");
+            appRegistry.registerAlgorithm(myPCA2);
+            myPCA2.setDocumentationURL(new Uri("http://en.wikipedia.org/wiki/Non-linear_iterative_partial_least_squares"));
+
+            //Add existing data sets 
         }
 
         void Application_End(object sender, EventArgs e)
@@ -32,8 +45,17 @@ namespace DataMiningApp
         void Session_Start(object sender, EventArgs e)
         {
             // Code that runs when a new session is started
-            stepid = 1;
+            // Session data
+            //Selected analysis
+            Session.Add("analysis", null);
+            Analysis.ParameterStream stream = new Analysis.ParameterStream(Session);
+            sessionRegistry = new Registry.Registry(Session);
             Session.Add("stepid", stepid);
+            stepid = 1;
+
+
+            //Add pre-existing user data sets
+            //Add pre-existing user-defined scripts
         }
 
         void Session_End(object sender, EventArgs e)
