c5b5c561aad23e544c5bedd51a344511dced4fcb
alejandro.asenjo <alejandro.asenjo@2500659b-82e6-3a7b-9723-c36c4ad48c0c>
2010-05-13 05:49:33 +0000

    Adding dynamic version of data select step

---
+++ DataMiningApp/Analysis/PCA/PCADynamicNIPALS.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DataMiningApp.Analysis.PCA
+{
+    public class PCADynamicNIPALS : Analysis
+    {
+
+        public PCADynamicNIPALS(String Name)
+            : base(Name)
+        {
+            setDocumentationURL(new Uri("http://en.wikipedia.org/wiki/Singular_value_decomposition"));
+            steps.Add(new Common.Steps.SelectDatasetDynamic());
+            steps.Add(new Steps.PCAConfig());
+            steps.Add(new Steps.NIPALS_PCA());
+            steps.Add(new Steps.PCAResults());
+
+        }
+
+        override public void run(HttpResponse response, System.Web.SessionState.HttpSessionState session)
+        {
+            base.response = response;
+            currentStep=0;
+            steps.ElementAt(currentStep).start(this, response, session);
+        }
+
+
+        override public void next(HttpResponse response, System.Web.SessionState.HttpSessionState session)
+        {
+            currentStep++;
+            steps.ElementAt(currentStep).start(this, response, session);
+        }
+    }
+}
