b004fe70014eccf7018692b5a34607b64eade470
alejandro.asenjo <alejandro.asenjo@2500659b-82e6-3a7b-9723-c36c4ad48c0c>
2010-05-13 07:29:43 +0000

    Changed default form to trigger algorithm step

--- DataMiningApp/Default.aspx.designer.cs
+++ DataMiningApp/Default.aspx.designer.cs
@@ -13,15 +13,6 @@ namespace DataMiningApp {
     public partial class _Default {
         
         /// <summary>
-        /// Head1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlHead Head1;
-        
-        /// <summary>
         /// main_form control.
         /// </summary>
         /// <remarks>
