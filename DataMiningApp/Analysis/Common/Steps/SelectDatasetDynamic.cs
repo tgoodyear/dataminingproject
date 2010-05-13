fda1a71693776f982778aacaf0a8640b138b07c6
alejandro.asenjo <alejandro.asenjo@2500659b-82e6-3a7b-9723-c36c4ad48c0c>
2010-05-13 05:49:33 +0000

    Adding dynamic version of data select step

---
+++ DataMiningApp/Analysis/Common/Steps/SelectDatasetDynamic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using System.Web;
+
+namespace DataMiningApp.Analysis.Common.Steps
+{
+    public class SelectDatasetDynamic : Step
+    {
+        Analysis caller;
+        public void start(Analysis parent, HttpResponse response, System.Web.SessionState.HttpSessionState session)
+        {
+            caller = parent;
+            Debug.WriteLine("SelectData step of " + caller.getDisplayName() + " started");
+            session["stepid"] = 1;
+            response.Redirect("~/Default.aspx");
+        }
+
+    }
+}
